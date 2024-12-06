cf0843fdcdffdcea436e0ca925e64a3c6a1ad2e3
Anastasiia Pukhalo <pukhalo.a@gmail.com>
2024-12-06 12:56:31 -0400

    updated label

--- src/widget/label.ts
+++ src/widget/label.ts
@@ -39,19 +39,19 @@ export class SKLabel extends SKElement {
   }
 
   protected _radius = 0;
-  set radius(r: number){
+  set radius(r: number) {
     this._radius = r;
   }
-  get radius(){
+  get radius() {
     return this._radius;
   }
 
   protected _font = Style.font;
-  set font(s: string){
+  set font(s: string) {
     this._font = s;
     this.setMinimalSize(this.width, this.height);
   }
-  get font(){
+  get font() {
     return this._font;
   }
 
@@ -72,10 +72,12 @@ export class SKLabel extends SKElement {
       return;
     }
 
-    this.height = height || m.height + this.padding * 2;
+     this.height = height || m.height + this.padding * 2;
 
-    this.width = width || m.width + this.padding * 2;
+     this.width = width || m.width + this.padding * 2;
+  
   }
+
 
   draw(gc: CanvasRenderingContext2D) {
     gc.save();
@@ -85,21 +87,21 @@ export class SKLabel extends SKElement {
 
     gc.translate(this.margin, this.margin);
 
-    if(this.fill){
+    if (this.fill) {
       gc.beginPath();
       gc.roundRect(this.x, this.y, w, h, this._radius);
-      gc.fillStyle =  this.fill;
+      gc.fillStyle = this.fill;
       gc.fill();
     }
-    
-    if(this.border){
+
+    if (this.border) {
       gc.strokeStyle = this.border;
       gc.lineWidth = 1;
       gc.stroke();
     }
-    
+
     // render text
-    gc.font = this._font;
+    /*gc.font = this._font;
     gc.fillStyle = this._fontColour;
     gc.textBaseline = "middle";
 
@@ -119,7 +121,34 @@ export class SKLabel extends SKElement {
         gc.fillText(this.text, this.x + w - this.padding, this.y + h / 2);
 
         break;
-    }
+    }*/
+
+    // render text with wrapping
+    gc.font = this._font;
+    gc.fillStyle = this._fontColour;
+    gc.textBaseline = "top";
+
+    const lines = this.wrapText(gc, this.text, (this.width || 0) - 2 * this.padding);
+    const lineHeight = measureText("M", this._font)?.height || 0; // Estimate line height using a reference character
+    let yOffset = this.y + this.padding;
+
+    lines.forEach((line) => {
+      switch (this.align) {
+        case "left":
+          gc.textAlign = "left";
+          gc.fillText(line, this.x + this.padding, yOffset);
+          break;
+        case "centre":
+          gc.textAlign = "center";
+          gc.fillText(line, this.x + w / 2, yOffset);
+          break;
+        case "right":
+          gc.textAlign = "right";
+          gc.fillText(line, this.x + w - this.padding, yOffset);
+          break;
+      }
+      yOffset += lineHeight;
+    });
 
     gc.restore();
 
@@ -127,6 +156,36 @@ export class SKLabel extends SKElement {
     super.draw(gc);
   }
 
+  wrapText(gc: CanvasRenderingContext2D, text: string, maxWidth: number): string[] {
+    const words = text.split(" ");
+    const lines: string[] = [];
+    let currentLine = "";
+
+    words.forEach((word) => {
+      const testLine = currentLine ? `${currentLine} ${word}` : word;
+      const textMetrics = measureText(testLine, this._font);
+
+      if (!textMetrics) {
+        console.warn(`measureText failed for: ${testLine}`);
+        return; // Skip processing this word if measureText fails
+      }
+
+      if (textMetrics.width > maxWidth) {
+        if (currentLine) {
+          lines.push(currentLine);
+        }
+        currentLine = word; // Start a new line with the current word
+      } else {
+        currentLine = testLine; // Add word to the current line
+      }
+    });
+
+    if (currentLine) {
+      lines.push(currentLine); // Push the last line
+    }
+
+    return lines;
+  }
   public toString(): string {
     return `SKLabel '${this.text}'`;
   }
